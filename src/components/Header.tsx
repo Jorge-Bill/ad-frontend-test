b556146d043f418301da42b44bb38640fe7d705a
Jorge Mendes <jorge.mendes@escala-app.com>
2024-12-20 22:13:32 -0300

    updates products

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 'use client';
 import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between bg-header px-4 py-10 sm:px-6 sm:py-8 lg:px-8">
-      <a href="/">
+      <Link href="/">
         <span className="sr-only">GamerShop</span>
         <Image
           src="/logo.svg"
@@ -15,12 +16,12 @@ export default function Header() {
           className="h-8 w-auto"
           priority
         />
-      </a>
+      </Link>
       <div className="hidden sm:flex sm:items-center sm:space-x-8">
-        <a href="/cart" className="-m-2 p-2 text-gray-400 hover:text-gray-500">
+        <Link href="/cart" className="-m-2 p-2 text-gray-400 hover:text-gray-500">
           <span className="sr-only">Cart</span>
           <ShoppingCartIcon aria-hidden="true" className="size-6" />
-        </a>
+        </Link>
       </div>
     </header>
   );
