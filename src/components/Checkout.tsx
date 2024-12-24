e6d16fab291018c5d933f32010b150f4f746bf46
Jorge Mendes <jorge.mendes@escala-app.com>
2024-12-24 11:52:22 -0300

    feat - add link to home page

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from 'react';
 import { Game } from '@/types/game';
+import Link from 'next/link';
+import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 import { getFromSession } from '@/services/session';
 import Card from './Card';
 import Notification from './Notification';
@@ -28,12 +30,22 @@ export default function Checkout() {
 
   return (
     <div className="mx-auto h-full min-h-screen max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h1 className="text-3xl sm:text-4xl font-bold tracking-tight text-gray-900">
+      <div className="mb-12 sm:flex sm:items-center sm:space-x-8">
+        <Link
+          href="/"
+          className="-m-2 flex items-center p-2 text-gray-600 hover:text-gray-500"
+        >
+          <span className="sr-only">Home</span>
+          <ArrowLeftIcon aria-hidden="true" className="mr-2 size-6" />
+          Back to Catalog
+        </Link>
+      </div>
+      <h1 className="text-6xl sm:text-4xl font-bold tracking-tight text-gray-900">
         Your Cart
         <p className="mt-1 font-normal text-gray-400">{`${cart.length} items`}</p>
       </h1>
       <form
-        action={() => alert('Checkout')}
+        action={() => alert('Proceed to Checkout')}
         className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16"
       >
         <section aria-labelledby="cart-heading" className="lg:col-span-7">
@@ -52,7 +64,9 @@ export default function Checkout() {
             ))}
             {cart.length === 0 && (
               <li className="text-sm flex py-6 transition-all duration-200 sm:py-10">
-                <p className="uppercase text-gray-500">Your cart is empty.</p>
+                <p className="text-capitalize text-center italic leading-6 text-gray-500 transition-all duration-200">
+                  Your cart is empty.
+                </p>
               </li>
             )}
           </ul>
